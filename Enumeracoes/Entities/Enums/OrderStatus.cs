ea481af7d4b852b262b8fb5354f2e165f7d696d7
Renanvt <jonatan_rvs@hotmail.com>
2020-06-08 16:10:25 -0300

    Criando as classes

---
+++ Enumeracoes/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Enumeracoes.Entities.Enums{
+    enum  OrderStatus : int
+    {
+        PendingPayment = 8,
+        Processing = 1,
+        Shippded = 2,
+        Delivered = 3
+        
+    }
+}
--- Enumeracoes/Program.cs
+++ Enumeracoes/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
 namespace Enumeracoes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            System.Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString(); 
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); //O Texto Delivered precisa estar igual ao do Enum OrderStatus
+            System.Console.WriteLine(os);
+            System.Console.WriteLine(txt);
+            //Imprime PedingPayment no formato de string
+            
+            //Esteriótipo na linguagem UML <<nome>>
         }
     }
 }
