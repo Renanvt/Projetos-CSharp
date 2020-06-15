ac6518705bba3b6027c3bc8029a1120e1146823a
Renanvt <jonatan_rvs@hotmail.com>
2020-06-15 18:35:06 -0300

    PROJETO FINALIZADO
    SEM ERROS

--- ComposicaoDeObjetos/Program.cs
+++ ComposicaoDeObjetos/Program.cs
@@ -10,63 +10,44 @@ namespace ComposicaoDeObjetos
     {
         static void Main(string[] args)
         {
-            int dep,contract;
-            string nome,level;
+            string deptName;
+            int N;
+            string nome;
             double sal;
             
-            Console.Write("Entre com o nome do departamento");
-            dep = int.Parse(Console.ReadLine());
-            Console.WriteLine("Entre com os dados do trabalhador");
+            Console.Write("Entre com o nome do departamento: ");
+            deptName = Console.ReadLine();
+            Console.WriteLine("Entre com os dados do trabalhador: ");
             Console.Write("Nome: ");
             nome = Console.ReadLine();
             Console.Write("Level (Junior/MidLevel/Senior): ");
-            level = Console.ReadLine();
-            bool levelCorreto = true;
-            WorkerLevel nivel;
-            while(levelCorreto){
-                if(level == "Junior"){
-                    nivel = Enum.Parse<WorkerLevel>("JUNIOR");
-                }
-                else if(level == "MidLevel"){
-                    nivel = Enum.Parse<WorkerLevel>("MID_LEVEL");
-                }
-                else if(level == "Senior"){
-                    nivel = Enum.Parse<WorkerLevel>("SENIOR");
-                }else{
-                    System.Console.WriteLine("Digite um nível válido");
-                    levelCorreto=false;
-                }
-            }
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
             Console.Write("Salário base: ");
             sal = Convert.ToDouble(Console.ReadLine(),CultureInfo.InvariantCulture);
-            Console.Write("Quantos contratos para esse trabalhador: ");
-            contract = int.Parse(Console.ReadLine());
-            
-            Worker trabalhador;
-            if(level=="Junior"){
-                trabalhador = new Worker(nome,WorkerLevel.JUNIOR,sal);
-            }else if(level=="MidLevel"){
-                trabalhador = new Worker(nome,WorkerLevel.MID_LEVEL,sal);
-            }else if(level == "Senior"){
-                trabalhador = new Worker(nome,WorkerLevel.SENIOR,sal);
-            }
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(nome,level,sal,dept);
 
-            string date="";
-            double valHr=0.0;
-            int dur=0;
-            
-            List<int> Contratos = new List<int>();
-            for(int i =0; i<contract;i++){
+            Console.Write("Quantos contratos para esse trabalhador: ");
+            N = int.Parse(Console.ReadLine());
+
+            for(int i =0; i<N;i++){
                 System.Console.WriteLine($"Entre com os dados do {i+1} contrato");
                 System.Console.Write("Data (DD/MM/YYYY): ");
-                date = Console.ReadLine();
+                DateTime date = DateTime.Parse(Console.ReadLine());
                 System.Console.Write("Valor por hora: ");
-                valHr = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                double valHr = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
                 System.Console.Write("Duracao (hours): ");
-                dur = int.Parse(Console.ReadLine());
-                trabalhador.addContract(dur)
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valHr,hours);
+                worker.AddContract(contract);
             }
-            
+            System.Console.Write("Entre com o mês e o ano para calcular o ganho do trabalhador (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month =int.Parse(monthAndYear.Substring(0,2));
+            int year= int.Parse(monthAndYear.Substring(3));
+            System.Console.WriteLine("Name: "+ worker.Name);
+            System.Console.WriteLine("Department: "+ worker.Department.Name);
+            System.Console.WriteLine("Income for "+monthAndYear + ": "+worker.Income(year,month).ToString("F2"),CultureInfo.InvariantCulture); 
         }
     }
 }
