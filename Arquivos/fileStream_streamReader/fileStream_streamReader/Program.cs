4f573a544a317f81e2c173bc424ccfd49bbc1710
Renanvt <jonatan_rvs@hotmail.com>
2020-07-01 01:42:45 -0300

    Criando o projeto

---
+++ Arquivos/fileStream_streamReader/fileStream_streamReader/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace fileStream_streamReader {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
