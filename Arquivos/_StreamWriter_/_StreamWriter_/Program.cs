8d86232938a2deae757db3fd49acd3075f4c5b5d
Renanvt <jonatan_rvs@hotmail.com>
2020-07-04 02:19:50 -0300

    Arquivo pronto

--- Arquivos/_StreamWriter_/_StreamWriter_/Program.cs
+++ Arquivos/_StreamWriter_/_StreamWriter_/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _StreamWriter_
 {
@@ -6,7 +7,30 @@ namespace _StreamWriter_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcePath = @"D:\Jonatan-SSD\OneDrive\Documentos\GitHub\Projetos-CSharp\Arquivos\_StreamWriter_\_StreamWriter_\file1.txt";
+            string targetPath = @"D:\Jonatan-SSD\OneDrive\Documentos\GitHub\Projetos-CSharp\Arquivos\_StreamWriter_\_StreamWriter_\file2.txt";
+            try
+            {
+                //Lendo e salvando em file2
+                //Lendo
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    //AppendText -> Abre o arquivo pra escrita, escrevendo a partir do final do arquivo, acresçentando no final
+                    foreach (string line in lines)
+                    {
+                        //escreve a string line no arquivo sw
+                        sw.WriteLine(line.ToUpper());
+
+                    }
+
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu!");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
