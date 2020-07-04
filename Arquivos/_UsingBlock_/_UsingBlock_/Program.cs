b63546ca2a7bb8d11395726da0b4ff64769bd38d
Renanvt <jonatan_rvs@hotmail.com>
2020-07-04 01:54:47 -0300

    using block

---
+++ Arquivos/_UsingBlock_/_UsingBlock_/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace _UsingBlock_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Jonatan-SSD\OneDrive\Documentos\GitHub\Projetos-CSharp\Arquivos\_UsingBlock_\_UsingBlock_\file1.txt";
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    //Deste modo o recurso instanciado será automaticamente fechado quando terminar o bloco
+                    using (StreamReader sr = new StreamReader(fs))  //cascateado os blocos
+                    //Pode fazer só um bloco instanciando tudo em uma linha:
+                    //using(StreamReader sr = File.OpenText(path)){...}
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+
+                }
+            }
+            catch(IOException e){
+                Console.WriteLine("Ocorreu um erro:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
