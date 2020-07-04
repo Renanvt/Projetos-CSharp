b9f6b6b9661c2fe6acb2bde7ea8da8b231546200
Renanvt <jonatan_rvs@hotmail.com>
2020-07-04 01:30:27 -0300

    Criando os arquivos

---
+++ Arquivos/FileStream_e_StreamReader/FileStream_e_StreamReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace FileStream_e_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Jonatan-SSD\OneDrive\Documentos\GitHub\Projetos-CSharp\Arquivos\FileStream_e_StreamReader\FileStream_e_StreamReader\arquivos\file1.txt";
+            //FileStream fs = null;
+            StreamReader sr = null;
+            try
+            {
+                //abre o arquivo
+                //fs = new FileStream(path, FileMode.Open);//FileStream(caminho, modo_de_instanciacao)
+                //sr = new StreamReader(fs);
+                sr = File.OpenText(path); // O .OpenText implicitamente ja instancia o FileStream e o StreamReader encima dele
+                string line = sr.ReadLine(); //Le uma linha do arquivo e armazena na variavel line
+                Console.WriteLine(line);
+
+                //Lendo todas as linhas do meu arquivo
+                while (!sr.EndOfStream) //Enquanto eu não chegar no final da Stream
+                {
+                    line = sr.ReadLine(); //Le uma linha do arquivo
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu:");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+               // if (fs != null) fs.Close();
+            }
+        }
+    }
+}
