98046b9cb2e3b53a0dd991bf6747c90f5b5af4b8
Renanvt <jonatan_rvs@hotmail.com>
2020-07-09 17:56:18 -0300

    Criando o exercicio

---
+++ Exercicios/exe02/exe02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace exe02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os dados do contrado: ");
+            Console.Write("Número: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Data (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Valor do contrato: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Entre com os numeros de parcelas: ");
+            int parcelas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Parcelas:");
+        }
+    }
+}
