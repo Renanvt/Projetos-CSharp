a7036377a19875f23c253adf7b578fddd6de5198
Renanvt <jonatan_rvs@hotmail.com>
2020-07-09 02:18:25 -0300

    Exercicios

--- Exercicios/exe01/exe01/Program.cs
+++ Exercicios/exe01/exe01/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using exe01.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
 namespace exe01
 {
@@ -6,7 +10,43 @@ namespace exe01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Pegar o caminho do projeto em armazenando em new_dir
+            string curDir = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+            string new_folder = @"\\..\\..\\..\out";
+
+            List<Item> list = new List<Item>();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Digite o nome do produto: ");
+                string nome = Console.ReadLine();
+                Console.Write("Digite o preco do produto: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Digite a quantidade: ");
+                int qtd = int.Parse(Console.ReadLine());
+           
+                list.Add(new Item(nome, preco, qtd));
+                Console.WriteLine("-----------------------");
+            }
+
+            Directory.CreateDirectory(curDir + new_folder);
+            string path = curDir + new_folder + @"\summary.csv";
+            try
+            {
+
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    foreach (var item in list)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Error:");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
---
+++ Exercicios/exe01/exe01/entities/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace exe01.Entities
+{
+    class Item
+    {
+        public string Nome { get; private set; }
+        public double Preco { get; private set; }
+        public int Qtde { get; private set; }
+        
+        public Item()
+        {
+
+        }
+
+        public Item(string nome, double preco, int qtde)
+        {
+            Nome = nome;
+            Preco = preco;
+            Qtde = qtde;
+        }
+
+        public double TotPreco()
+        {
+            Preco = Preco * Qtde;
+            return Preco;
+        }
+        public override string ToString()
+        {
+           
+            return $"{Nome},{TotPreco().ToString("f2",CultureInfo.InvariantCulture)}";
+        }
+
+    }
+}
