e92af6fdf3cc156c986f12e862ee7b8624575b0b
Renanvt <jonatan_rvs@hotmail.com>
2020-07-11 16:37:43 -0300

    Com Generics e IComparable

--- GenericsRestricoes/GenericsRestricoes/CalculationService.cs
+++ GenericsRestricoes/GenericsRestricoes/CalculationService.cs
@@ -6,16 +6,16 @@ namespace GenericsRestricoes
 {
     class CalculationService
     {
-        public int Max(List<int> list) //Retorna o maior elemento da lista
+        public T Max<T>(List<T> list) where T : IComparable //A minha lista vai ter que ser de um tipo "T" qualquer Onde esse tipo seja IComparable
         {
             if(list.Count == 0) // Se a quantidade de elementos na minha lista ==0
             {
                 throw new ArgumentException("A lista não pode estar vazia");
             }
-            int max = list[0];// Primeiro elemento da lista
+            T max = list[0];// Primeiro elemento da lista
             for(int i =1; i< list.Count; i++)
             {
-                if(list[i] > max)
+                if(list[i].CompareTo(max) > 0) //Compara o list[i] com max, verificando se list[i] é maior do que max
                 {
                     max = list[i];
                 }
