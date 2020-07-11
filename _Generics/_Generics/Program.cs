ab8523b6f62a5be0005f7509e158a4df84b8afc0
Renanvt <jonatan_rvs@hotmail.com>
2020-07-11 15:51:09 -0300

    Sem type safity - int

--- _Generics/_Generics/Program.cs
+++ _Generics/_Generics/Program.cs
@@ -8,7 +8,8 @@ namespace _Generics
         static void Main(string[] args)
         {
             //Generics permites que classes, interfaces e métodos possam ser parametrizados por tipo. Seus benefícios são: Reuso, type safety, performance
-            //PrintService printService = new PrintService();
+            //PrintServiceString printService = new PrintServiceString();
+            //Essa solução não tem Type safety
             PrintService printService = new PrintService();
             Console.Write("Quantas valores? ");
             int n = int.Parse(Console.ReadLine());
@@ -17,7 +18,11 @@ namespace _Generics
                 int x = int.Parse(Console.ReadLine());
                 printService.AddValue(x);
             }
+            int a = (int) printService.First(); //Não pode converter implicitamento objeto pra int
+            int b = a + 2;
+            Console.WriteLine(b);
             printService.Print();
+            Console.WriteLine();
             Console.WriteLine("Primeiro: "+printService.First());
         }
     }
