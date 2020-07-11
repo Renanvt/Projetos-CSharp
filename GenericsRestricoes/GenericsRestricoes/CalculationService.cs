29951691d3e743658703e6d3506e8b36a9d1c577
Renanvt <jonatan_rvs@hotmail.com>
2020-07-11 16:28:49 -0300

    Sem Generics, Versao Light (int)

---
+++ GenericsRestricoes/GenericsRestricoes/CalculationService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsRestricoes
+{
+    class CalculationService
+    {
+        public int Max(List<int> list) //Retorna o maior elemento da lista
+        {
+            if(list.Count == 0) // Se a quantidade de elementos na minha lista ==0
+            {
+                throw new ArgumentException("A lista não pode estar vazia");
+            }
+            int max = list[0];// Primeiro elemento da lista
+            for(int i =1; i< list.Count; i++)
+            {
+                if(list[i] > max)
+                {
+                    max = list[i];
+                }
+            }
+            return max; //Maior elemento da lista
+        }
+    }
+}
---
+++ GenericsRestricoes/GenericsRestricoes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace GenericsRestricoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(); //Cria uma lista de numeros inteiros e inicia vazia
+            Console.Write("Entre com o número de produtos: ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i = 0; i< n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+            CalculationService calculationService = new CalculationService();
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max: ");
+            Console.WriteLine(max);
+        }
+    }
+}
