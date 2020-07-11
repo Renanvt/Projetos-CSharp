fbc6a9753e03dce381809f11cd88953c23e66beb
Renanvt <jonatan_rvs@hotmail.com>
2020-07-11 18:21:53 -0300

    GetHashCode diferente

--- Equals/Equals/Program.cs
+++ Equals/Equals/Program.cs
@@ -8,8 +8,10 @@ namespace Equals
         static void Main(string[] args)
         {
             Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
-            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com" };
             Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
 
         }
     }
