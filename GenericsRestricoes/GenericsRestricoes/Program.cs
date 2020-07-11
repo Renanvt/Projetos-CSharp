d706297349110e21d6d85372134ef42c399eb682
Renanvt <jonatan_rvs@hotmail.com>
2020-07-11 16:53:35 -0300

    Comparando Produtos com Generics e ComparteTo

--- GenericsRestricoes/GenericsRestricoes/Program.cs
+++ GenericsRestricoes/GenericsRestricoes/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
+using GenericsRestricoes.Services;
+using GenericsRestricoes.Entities;
+using System.Globalization;
 
 namespace GenericsRestricoes
 {
@@ -8,16 +10,18 @@ namespace GenericsRestricoes
     {
         static void Main(string[] args)
         {
-            List<int> list = new List<int>(); //int implementa IComparable
+            List<Product> list = new List<Product>(); 
             Console.Write("Entre com o número de produtos: ");
             int n = int.Parse(Console.ReadLine());
             for(int i = 0; i< n; i++)
             {
-                int x = int.Parse(Console.ReadLine());
-                list.Add(x);
+                string[] vect = Console.ReadLine().Split(',');
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
             }
             CalculationService calculationService = new CalculationService();
-            int max = calculationService.Max(list);
+            Product max = calculationService.Max(list);
 
             Console.WriteLine("Max: ");
             Console.WriteLine(max);
--- GenericsRestricoes/GenericsRestricoes/Services/CalculationService.cs
+++ GenericsRestricoes/GenericsRestricoes/Services/CalculationService.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GenericsRestricoes.Services;
 
-namespace GenericsRestricoes
+namespace GenericsRestricoes.Services
 {
     class CalculationService
     {
