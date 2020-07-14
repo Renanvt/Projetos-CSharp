76a3a81e24ca04930f5cceea83792a2a06a76641
Renanvt <jonatan_rvs@hotmail.com>
2020-07-14 15:59:43 -0300

    HashSet Add e Contains

---
+++ HashSet_SortedSet/HashSet_SortedSet/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace HashSet_SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instanciando um conjunto vazio
+            HashSet<string> set = new HashSet<string>();
+            //Inserindo elementos no conjunto
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+
+            //Testando se Notebook existe no conjunto
+            Console.WriteLine(set.Contains("Notebook"));
+        }
+    }
+}
