2fa72bead2b783721d09ed0ed5e6159a2f5ae0fe
Renanvt <jonatan_rvs@hotmail.com>
2020-07-16 17:03:20 -0300

    Tipo referencia

---
+++ Hash_Igualdade/Hash_Igualdade/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hash_Igualdade.Entities
+{
+    class Product //Tipo referencia
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Hash_Igualdade/Hash_Igualdade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Hash_Igualdade.Entities;
+namespace Hash_Igualdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+
+            //Se GetHashCode e Equal NÃO estiverem implementados:
+            //Tipos referência: compara as referencias do objeto
+            //Tipos valor: compara os valores dos atributos
+
+            a.Add(new Product("TV", 900.0));
+            a.Add(new Product("Notebook", 1200.0));
+
+            HashSet<Point> b = new HashSet<Point>();
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1200.0); //prod tem endereço de memória diferente de a
+            Console.WriteLine(a.Contains(prod));
+        }
+    }
+}
