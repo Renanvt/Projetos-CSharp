6e3a486fb9ce0ee7640ae2ed0f168bb01d487394
Renanvt <jonatan_rvs@hotmail.com>
2020-07-17 03:15:34 -0300

    size, foreach, dictionary[key], remove

--- _DictionaryESortedDictionary_/_DictionaryESortedDictionary_/Program.cs
+++ _DictionaryESortedDictionary_/_DictionaryESortedDictionary_/Program.cs
@@ -7,13 +7,36 @@ namespace _DictionaryESortedDictionary_
     {
         static void Main(string[] args)
         {
+            //DICTIONARY[KEY]
             Dictionary<string, string> cookies = new Dictionary<string, string>();
-            cookies["user"] = "maria"; //Cookies na chave "user" recebe "maria"
+            cookies["user"] = "maria"; //Cookies na chave "user" recebe "maria" 
             cookies["email"] = "maria@gmail.com";
             cookies["phone"] = "99712234"; //Esse valor é sobreescrito pelo próximo valor
             cookies["phone"] = "52842158"; //Esse valor sobreescreve encima da key phone
+            Console.WriteLine(cookies["email"]);
+            Console.WriteLine(cookies["phone"]); //Imprime 52842158
 
-            Console.WriteLine(cookies["phone"]);
+            cookies.Remove("email");
+            //Console.WriteLine(cookies["email"]); //Erro, Tentando acessar uma chave que não existe
+            if (cookies.ContainsKey("email"))//false
+            {
+                Console.WriteLine(cookies["email"]);
+            }
+            else
+            {
+                Console.WriteLine("Nada encontrado no key 'email' ");//Exibe essa mensagem
+            }
+
+            //Testando o tamanho do Dictionary
+            Console.WriteLine("Size: "+cookies.Count); //2 -> user, phone
+            Console.WriteLine("Todos os cookies: ");
+            
+            //Percorrendo o Dictionary
+            foreach (KeyValuePair<string, string> item in cookies)
+            { //Para cada item do tipo KeyValuePair, Estou percorrendo cada um dos pares do tipo string, string
+                Console.WriteLine(item.Key + ": " +item.Value); //Imprimindo a chave e o valor 
+            }
+                
         }
     }
 }
