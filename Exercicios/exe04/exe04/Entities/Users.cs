42b814abead4c8d67956cb3cb36c7e538c72fcc8
Renanvt <jonatan_rvs@hotmail.com>
2020-07-17 02:22:04 -0300

    Lendo quantidade de usuários

---
+++ Exercicios/exe04/exe04/Entities/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exe04.Entities
+{
+    class Users
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public Users(string userName, DateTime instant)
+        {
+            UserName = userName;
+            Instant = instant;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Users))
+            {
+                return false;
+            }
+            return obj is Users users &&
+                   UserName == users.UserName;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+    }
+}
---
+++ Exercicios/exe04/exe04/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using exe04.Entities;
+
+namespace exe04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Users> set = new HashSet<Users>();
+            Console.Write("Entre com o caminho da pasta: ");
+            
+            string path = Console.ReadLine();
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    Console.WriteLine("Total de usuários: ");
+                    while (!sr.EndOfStream)
+                    {
+                        //Readline -> Le uma linha
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        
+                        set.Add(new Users(name, instant));
+                    }
+                    
+                    Console.WriteLine(set.Count);
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Error:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
