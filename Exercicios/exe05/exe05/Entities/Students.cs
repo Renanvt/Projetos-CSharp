bfc713e9f6f6d21893f18ee8224c2a30e0dd100a
Renanvt <jonatan_rvs@hotmail.com>
2020-07-17 02:51:17 -0300

    Criando as classes e comparando (Programa Pronto)

---
+++ Exercicios/exe05/exe05/Entities/Students.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exe05
+{
+    class Students
+    {
+        public int Id { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Students))
+            {
+                return false;
+            }
+            return obj is Students students &&
+                   Id == students.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ Exercicios/exe05/exe05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exe05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Students> set = new HashSet<Students>();
+            char[] course = new char[3];
+            course[0] = 'A';
+            course[1] = 'B';
+            course[2] = 'C';
+            int i=0, j;
+            do
+            {
+                Console.Write($"Informe a quantidade de estudantes do curso {course[i]}: ");
+                int n = int.Parse(Console.ReadLine());
+                for (j = 0; j < n; j++)
+                {
+                    set.Add(new Students { Id = int.Parse(Console.ReadLine()) });
+                }
+                i++;
+            } while (i < 3);
+            Console.Write("Total de estudantes: ");
+            Console.WriteLine(set.Count);
+        }
+    }
+}
