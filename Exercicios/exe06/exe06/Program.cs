3fcca4098ccc8882cfaf4b655a40a530066839a8
Renanvt <jonatan_rvs@hotmail.com>
2020-07-18 02:27:10 -0300

    ExtensionMethod [1]

--- Exercicios/exe06/exe06/Program.cs
+++ Exercicios/exe06/exe06/Program.cs
@@ -1,4 +1,4 @@
-﻿using exe06.Entities;
+﻿
 using System;
 using System.Collections.Generic;
 using System.IO;
---
+++ ExtensionMethod/ExtensionMethod/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ExtensionMethod.Extensions
+{
+   static class DateTimeExtensions
+    {
+        //extension methods
+        public static string ElapsedTime(this DateTime thisObj)
+            //ElapsedTime está extendendo um tipo DateTime
+        {
+            //Transforma a data em uma duração em data ou em dias em relação ao instante atual
+            TimeSpan duration = DateTime.Now.Subtract(thisObj);
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("f1", CultureInfo.InvariantCulture) + " days";
+            }
+        }
+    }
+}
