ef8c177194b2cff10770df8471744ccbfc906565
Renanvt <jonatan_rvs@hotmail.com>
2020-07-18 16:31:57 -0300

    Função sem transparencia, com variavel global

---
+++ SemTransparenciaReferencial/SemTransparenciaReferencial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SemTransparenciaReferencial
+{
+    class Program
+    {
+        public static int globalValue = 3;
+        static void Main(string[] args)
+        {
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues(vect); //Mude os valores ímpares do vetor
+            Console.WriteLine(string.Join(" ",vect)); //Join -> Pega cada elemento do meu vetor e juntar no string separado por espaços
+
+        }
+        public static void ChangeOddValues(int[] numbers)
+        {
+            for(int i = 0; i<numbers.Length; i++) //Percorre o vetor
+            {
+                if(numbers[i] % 2 != 0) //Se o elemento do vetor na posição i for um número impar
+                {
+                    numbers[i] += globalValue;
+                }
+            }
+        }
+    }
+}
--- _Comparison_T_/_Comparison_T_/Program.cs
+++ _Comparison_T_/_Comparison_T_/Program.cs
@@ -14,17 +14,31 @@ namespace _Comparison_T_
             list.Add(new Product("Notebook", 120.00));
             list.Add(new Product("Tablet", 450.00));
 
-            list.Sort(CompareProducts); //Só funciona se o tipo da lista implementa a interface IComparable
+            /*Comparison<Product> comp = CompareProducts; Variável que guarda uma referencia para uma função
+            list.Sort(comp);*/
+
+            /*Expressão lambida, função anônima
+             * Comp = (<argumentos da função>) <OperadorDeFuncao> <ExpressaoResultanteDosArgumentos>
+             */
+           // Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+           // list.Sort(comp);
+
+            /*Expressão lambida dentro da função Sort*/
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+            //list.Sort(CompareProducts); //Só funciona se o tipo da lista implementa a interface IComparable
+            //CompareProducts é uma referência para a função Sort - Delegate
 
             foreach(Product p in list)
             {
                 Console.WriteLine(p);
             }
+            
         }
-        static int CompareProducts(Product p1, Product p2)
+       /* static int CompareProducts(Product p1, Product p2)
         {
             //Vai fazer o mesmo papel do IComparable
             return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
         }
+       */
     }
 }
