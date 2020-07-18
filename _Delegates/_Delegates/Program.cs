f41f1ea4b6d6f70f49122e6f7bc9f525f5da52f1
Renanvt <jonatan_rvs@hotmail.com>
2020-07-18 17:07:34 -0300

    Delegate

---
+++ _Delegates/_Delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Delegates
+{
+    //DECLARAÇÃO DO DELEGATE
+    delegate double BinaryNumericOperation(double n1, double n2);//Operação numérica binária
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.Sum; //O meu objeto op é uma referência para a função
+            double result = op(a, b); //Exibe 22
+            Console.WriteLine(result);
+        }
+    }
+}
