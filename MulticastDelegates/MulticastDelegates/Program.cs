479ae79a7bddfcd50a3c99042570c698712c214a
Renanvt <jonatan_rvs@hotmail.com>
2020-07-20 18:53:28 -0300

    Comentando

--- MulticastDelegates/MulticastDelegates/Program.cs
+++ MulticastDelegates/MulticastDelegates/Program.cs
@@ -10,9 +10,15 @@ namespace MulticastDelegates
             double a = 10;
             double b = 12;
             BinaryNumericOperation op = CalculationService.ShowSum; //Op recebe a referencia da função
-            op += CalculationService.ShowMax; //Acrecenta a referencia de outra função
+            op += CalculationService.ShowMax; //Acrescenta a referencia de outra função
             op.Invoke(a, b);
-            
+            /*
+             * Exibe:
+             * 22 -> Resultado da função ShowSum
+             * 12 -> Resultado da função ShowMax
+             * Sintaxe alternativa:
+             * op(a,b); -> Exibe o mesmo resultado
+             */
         }
     }
 }
