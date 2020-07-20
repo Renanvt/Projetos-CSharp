9eeca37a67ef51c468eef0d499835f922c2408e3
Renanvt <jonatan_rvs@hotmail.com>
2020-07-20 18:40:49 -0300

    MulticastDelegate

---
+++ MulticastDelegates/MulticastDelegates/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MulticastDelegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+            BinaryNumericOperation op = CalculationService.ShowSum; //Op recebe a referencia da função
+            op += CalculationService.ShowMax; //Acrecenta a referencia de outra função
+            op.Invoke(a, b);
+            
+        }
+    }
+}
