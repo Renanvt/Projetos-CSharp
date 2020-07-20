6feaad33907abf24006a6e2277297007c1e71d43
Renanvt <jonatan_rvs@hotmail.com>
2020-07-20 18:10:13 -0300

    Delegates

--- _Delegates/_Delegates/Program.cs
+++ _Delegates/_Delegates/Program.cs
@@ -3,7 +3,7 @@
 namespace _Delegates
 {
     //DECLARAÇÃO DO DELEGATE
-    delegate double BinaryNumericOperation(double n1, double n2);//Operação numérica binária
+    delegate double BinaryNumericOperation(double n1, double n2);//Operação numérica binária, Só recebe 2 double como parâmetros e não casa com a função Square()
     class Program
     {
         static void Main(string[] args)
@@ -11,8 +11,14 @@ namespace _Delegates
             double a = 10;
             double b = 12;
             BinaryNumericOperation op = CalculationService.Sum; //O meu objeto op é uma referência para a função
-            double result = op(a, b); //Exibe 22
+            double result = op.Invoke(a, b); //Exibe 22, Aplicou a função sum nos operadores a e b
+            //Invoke -> Invoca a função sum
             Console.WriteLine(result);
+
+            /*Sintaxe alternativa:
+              BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+             */
+
         }
     }
 }
