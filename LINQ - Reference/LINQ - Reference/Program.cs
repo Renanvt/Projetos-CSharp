61e3ea789bd3243cf25b3d8dbd2411884e7d7d11
Renanvt <jonatan_rvs@hotmail.com>
2020-07-21 11:17:55 -0300

    Comentados

---
+++ LINQ - Reference/LINQ - Reference/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LINQ___Reference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /*
+            * • Filtering: Where, OfType 
+            * • Sorting: OrderBy, OrderByDescending, ThenBy, ThenByDescending, Reverse
+            * • Set: Distinct, Except, Intersect, Union 
+            * • Quantification: All, Any, Contains 
+            * • Projection: Select, SelectMany
+            * • Partition: Skip, Take 
+            * • Join: Join, GroupJoin
+            * • Grouping: GroupBy 
+            * • Generational: Empty 
+            * • Equality: SequenceEquals 
+            * • Element: ElementAt, First, FirstOrDefault Last, LastOrDefault, Single, SingleOrDefault 
+            * • Conversions: AsEnumerable, AsQueryable 
+            * • Concatenation: Concat
+            * • Aggregation: Aggregate, Average, Count, LongCount, Max, Min, Sum
+           */
+        }
+    }
+}
