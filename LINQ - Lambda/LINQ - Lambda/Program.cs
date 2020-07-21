1c3554da76240871a53b86d81b9e3890a18fbedc
Renanvt <jonatan_rvs@hotmail.com>
2020-07-21 14:11:06 -0300

    Operações com list - Parte 1

--- LINQ - Lambda/LINQ - Lambda/Program.cs
+++ LINQ - Lambda/LINQ - Lambda/Program.cs
@@ -52,7 +52,76 @@ namespace LINQ___Lambda
              Level
              */
 
-            var r3 
+            var r3 = products.Where(p => p.Name[0] == 'C').Select(p => new { p.Name, p.Price, CategoryName = p.Category.Name});
+            //CategoryName -> Apelido para não haver ambiguidade
+            //Pega os produtos cujo nome começa com a letra C
+            //Objeto anônimo tem o .ToString padrão
+            Print("Names começado com a letra 'C' e Objeto anônimo: ", r3);
+            /*
+             * Exibe:
+             * { Name = Computer, Price = 1100, CategoryName = Computers }
+             * { Name = Camera, Price = 700, CategoryName = Electronics }       
+             */
+
+            var r4 = products.Where(p => p.Category.Tier == 1).OrderBy(p => p.Price).ThenBy(p => p.Name);
+            Print("Tier 1, Ordenado por Preço e então ordenado por Nome: ", r4);
+            /*
+             * Exibe:
+             8. Printer, 350.00, Electronics, 1
+             7, Camera, 700.00, Electronics, 1  
+             10, Sound Bar, 700.00, Electronics, 1
+             6, Tablet, 700.00, Computers, 1
+             1, Computer, 1100.00, Computers, 1
+             4, Notebook, 1300.00, Computers, 1
+             3, TV, 1700.00, Electronics, 1
+             9, MacBook, 1800.00, Computers, 1
+             
+             Explicação: Ordena por preço, Depois quando empata os preços 700.00 -> 700.00, Ordena por nome
+             */
+
+            var r5 = r4.Skip(2).Take(4); //Pule os dois primeiroos e pegue 4 elementos
+            Print("Tier 1, Ordenado por Preço e então ordenado por Nome, pule 2 elementos e pegue 4 elementos", r5);
+            /*
+             * Exibe:
+             10, Sound Bar, 700.00, Electronics, 1
+             6, Tablet, 700.00, Computers, 1
+             1, Computer, 1100.00, Computers, 1
+             4, Notebook, 1300.00, Computers, 1
+             */
+
+            var r6 = products.First();
+            Console.WriteLine("Teste 1 - primero: " + r6); //Exibe o primeiro elemento do DataSource:
+            /*
+             * Exibe:
+             * Teste 1 - primeiro: 1, computer, 1100.00, Computers, 1
+             
+             */
+            //var r7 = products.Where(p => p.Price > 3000.0).First();
+            //Where -> Vai Retornar vazio, pois não tem nenhum preço acima de 3000.0
+           // Console.WriteLine("Teste 2 - primeiro: "+r7); //Vai dar Erro de Exceção
+            
+            var r8 = products.FirstOrDefault();
+            Console.WriteLine("Teste 3 - Primeiro ou retorne empty " + r8); //Retorna 1, Computer, 1100.00, Computers, 1
+            var r9 = products.Where(p => p.Price > 3000.0).FirstOrDefault();
+            Console.WriteLine("Teste 4 - Primeiro ou retorne empty" + r9); //Retorna empty {vazio}
+            Console.WriteLine();
+
+            var r10 = products.Where(p => p.ID == 3 || p.ID == 4); //Retorna uma coleção
+            Print("Sem single or default:", r10);
+            /*Exibe:
+             3, TV, 1700.00, Electronics, 1
+             4, Notebook, 1300.00, Computers, 1
+             */
+       
+            var r11 = products.Where(p => p.ID == 3).SingleOrDefault(); //devolve 1 resultado ou retorna vazio se não encontrar
+            //Só usar SingleOrDefault se tiver certeza que irá retornar ou 1, ou nenhum elemento
+            Console.WriteLine("Single or Default teste1: "+ r11);
+            //Retorna o objeto do tipo produt, onde ID==3
+            //3, TV, 1700.00, Electronics, 1
+
+            var r12 = products.Where(p => p.ID == 30).SingleOrDefault();
+            Console.WriteLine("Single or Default teste2: " + r12);
+            //Retorna vazio
         }
     }
 }
