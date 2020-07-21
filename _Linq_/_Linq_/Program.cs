12976badc1325210122a88147ae528f805b9514b
Renanvt <jonatan_rvs@hotmail.com>
2020-07-21 11:02:17 -0300

    linq coment

--- _Linq_/_Linq_/Program.cs
+++ _Linq_/_Linq_/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 namespace _Linq_
 {
@@ -9,15 +10,19 @@ namespace _Linq_
             //Especificar o data source
             int[] numbers = new int[] { 2, 3, 4, 5 };
             //Definir a expressão de query
-            var result = numbers
+            IEnumerable<int> result = numbers
                 .Where(x => x % 2 == 0)
                 .Select(x => x * 10); //Gera uma nova coleção contendo somente os números pares
                 //Where -> Filtra a fonte de dados
                 //Select -> Coloca a função para cada elemento da coleção
+            //Pega somente os elementos pares da fonte de dados, e multiplica por 10
             //Executa a query
             foreach(int x in result)
             {
                 Console.WriteLine(x);
+                /*Exibe:
+                 20
+                40*/
             }
         }
     }
