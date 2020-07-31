f9664b8b59553a8e316e2408a7133313c30d0b4b
Renanvt <jonatan_rvs@hotmail.com>
2020-07-31 12:27:16 -0300

    Exercicio Pronto

---
+++ Exercicios/exe08/exe08/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace exe08.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public override string ToString()
+        {
+            return Name
+                +","
+                +Email
+                +","
+                +Salary.ToString("f2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Exercicios/exe08/exe08/Program.cs
@@ -0,0 +1,61 @@
+﻿using exe08.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace exe08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o caminho da pasta: ");
+            string path = Console.ReadLine();
+            List<Employee> list = new List<Employee>();
+            try
+            {
+                using(StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2],CultureInfo.InvariantCulture);
+                        list.Add(new Employee()
+                        {
+                            Name = name,
+                            Email = email,
+                            Salary = salary
+                        });
+                    }
+                    Console.Write("Entre com o salário: ");
+                    double salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                    Console.WriteLine($"Email das pessoas com o salário acima de {salario.ToString("f2",CultureInfo.InvariantCulture)}");
+                    var emailProcurado =
+                        (from p in list
+                         where p.Salary > salario
+                         orderby p.Name
+                         select p.Email);
+                    foreach(var obj in emailProcurado)
+                    {
+                        Console.WriteLine(obj);
+                    }
+                    var sumSalary =
+                        (from p in list
+                         where p.Name[0] == 'M'
+                         select p).Sum(p => p.Salary);
+                    Console.WriteLine("Soma de salário de pessoas cujo o nome começa com 'M': "+sumSalary.ToString("f2",CultureInfo.InvariantCulture));
+                }
+            }
+            catch(IOException e)
+            {
+                Console.Write("Error: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
